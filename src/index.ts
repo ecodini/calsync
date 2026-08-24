5723902c7acd10b4520ece6e4856c610bc0e0fc8
Enzo Codini <ecodini@fi.uba.ar>
2026-08-24 15:55:36 -0300

    super cool

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,13 @@ import cron from 'node-cron';
 
 console.log("Calendar sync service stated");
 
+syncCalendar().catch(err => {
+    console.error("Error syncing calendar:", err);
+});
+
 // run every 2 hours from 8am to 8pm, Monday to Friday
 cron.schedule('0 8-20/2 * * 1-5', () => {
     syncCalendar().catch(err => {
         console.error("Error syncing calendar:", err);
     });
-}, { noOverlap: true });
+}, { noOverlap: true, timezone: process.env.TZ || 'America/Argentina/Buenos_Aires' });
